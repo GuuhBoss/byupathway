59542df10e467c7ba8e4db8579f3f876c1b57e0d
Gustavo Silva <gustavo.silva@toogas.com>
2025-07-01 09:15:28 +0100

    BYU Pathway - [Software Developer] - Week 1

---
+++ Week 1/Project01.py
@@ -0,0 +1,12 @@
+adjective = input("Enter an adjective: ")
+animal = input("Enter an animal: ")
+verb1 = input("Enter a verb: ")
+exclamation = input("Enter an exclamation: ")
+verb2 = input("Enter another verb: ")
+verb3 = input("Enter another verb: ")
+
+print(f"""Last week, something strange happened.
+I was walking home when I noticed a {adjective} {animal} {verb1} in the middle of the sidewalk.
+\"{exclamation}!\" I shouted.
+Without thinking, I started to {verb2} as loudly as I could.
+Somehow, that made it {verb3} in place, and then it decided to leap right over a trash can and disappear!""")
