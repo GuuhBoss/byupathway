c15abeaebbc7b428cc6c5bf69c5e7649c96b1b9d
Gustavo Silva <gustavo.silva@toogas.com>
2025-07-04 15:11:53 +0100

    added capitalize, and 2 new variables and setences

--- Week 1/Project01.py
+++ Week 1/Project01.py
@@ -4,9 +4,13 @@ verb1 = input("Enter a verb: ")
 exclamation = input("Enter an exclamation: ")
 verb2 = input("Enter another verb: ")
 verb3 = input("Enter another verb: ")
+story_object = input("Enter an object: ")
+sound = input("Enter a sound: ")
 
 print(f"""Last week, something strange happened.
 I was walking home when I noticed a {adjective} {animal} {verb1} in the middle of the sidewalk.
-\"{exclamation}!\" I shouted.
+\"{exclamation.capitalize()}!\" I shouted.
 Without thinking, I started to {verb2} as loudly as I could.
-Somehow, that made it {verb3} in place, and then it decided to leap right over a trash can and disappear!""")
+Somehow, that made it {verb3} in place, and then it decided to leap right over a trash can and disappear!
+Then, out of nowhere, it dropped a {story_object} that made a loud "{sound}" when it hit the ground.
+I just stood there, questioning my life choices.""")
